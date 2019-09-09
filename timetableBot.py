325a566192b4cb30df7e3255d86fb422d41dedd9
Lorenzo <lore.sarti.93@gmail.com>
2019-09-09 17:06:58 +0200

    Code Refactoring

--- timetableBot.py
+++ timetableBot.py
@@ -3,173 +3,192 @@ import json
 from telegram import (ReplyKeyboardMarkup)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
 import telegram
-import time,datetime
+import datetime as dt
 
 
 ### GET CONNECTION INFOS ###
-with open('bot_info.json', 'r') as f:
-    connection_data = json.load(f)
+def getConnectionData():
+    with open('bot_info.json', 'r') as f:
+        return json.load(f)
 
 
-#Bot unifi:
-# /orario -Oggi o -Domani
-# /news   -> prese dal sito unifi (DA SVILUPPARE)
-
-updater= Updater(token=connection_data["token"])  
-dispatcher= updater.dispatcher
-SELECT,CHOOSE = range(2)
+connection_data = getConnectionData()
+updater = Updater(token=connection_data["token"])
+dispatcher = updater.dispatcher
+SELECT, CHOOSE = range(2)   # Conversation states
 
 
-###MATERIE#########################
-QAS = '*Quantitative Analysis of Systems*'
-CPS = '*Cyber Physical Systems*'
-QC  = '*Quality and Certification*'
-ANA = '*Advance Numerical Analysis*'
-IoT = '*Distributed Programming for IoT*'
-
-#####ORARIO########################
-lunedi= '09:30-11:15'.ljust(15,'.')+CPS+'\n'+'11:30-13:00'.ljust(15,'.')+QAS+'\n'+'14:30-17:30'.ljust(15,'.')+IoT
-martedi= '09:30-11:15'.ljust(15,'.')+CPS+' - ROOM 221'+'\n'+'11:30-13:00'.ljust(15,'.')+QAS+'\n'+'15:30-18:00'.ljust(15,'.')+QC
-mercoledi= '9:00-10:30'.ljust(15,'.')+ANA+'\n'+'14:30-17:30'.ljust(15,'.')+IoT
-giovedi= '10:30-13:00'.ljust(15,'.')+QAS+'\n'+'14:30-17:30'.ljust(15,'.')+CPS
-venerdi= '10:45-13:15'.ljust(15,'.')+ANA+'\n'+'14:30-17:15'.ljust(15,'.')+QC
-sabato= '*No Lectures!*'
-domenica= '*No Lectures!*'
-
-#####DAYS#########################
-mon = 'Lun - Mon'
-tue = 'Mar - Tue'
-wed = 'Mer - Wed'
-thu = 'Gio - Thu'
-fri = 'Ven - Fri'
-
-##################################
-
-padding = 28
-symb = '+'
-
-timetable= { 
-			 0:'Sun'.center(padding,symb)+'\n'+domenica,	
-			 1:mon.center(padding,symb)+'\n'+lunedi,
-			 2:tue.center(padding,symb)+'\n'+martedi,
-			 3:wed.center(padding,symb)+'\n'+mercoledi,
-			 4:thu.center(padding,symb)+'\n'+giovedi,
-			 5:fri.center(padding,symb)+'\n'+venerdi,
-			 6:'Sat'.center(padding,symb)+'\n'+sabato
-			}
-
-def getDayCode(day):	#caso di scelta automatica
-	if day=='Oggi\nToday':
-		return 0
-	else:
-		return 1
-
-def setupDayCode(day):	#giorno scelto dall'utente
-	if day == mon:
-		return 1
-	elif day == tue:
-		return 2
-	elif day == wed:
-		return 3
-	elif day == thu:
-		return 4
-	elif day == fri:
-		return 5
-	else:
-		return 6
+### GET TIMETABLE ###
+def getRawTimetable():
+    raw_timetable = []
+    with open('timetable.json', 'r') as f:
+        raw_timetable = json.load(f)
+    return raw_timetable
 
 
-def getTimetable(oggi, richiesta):
-	giorno= (int(oggi)+richiesta) % 7  #modulo 7 per ricominciare dal lunedì
-	table=timetable[giorno]
-	return table
+def getLectureInfos(course_acr, lecture):
+    lecture_infos = "{}-{}".format(lecture["start"], lecture['end']).ljust(15, '.')
+    lecture_infos += "*{}* -> {}\n".format(course_acr, lecture["place"])
+    return lecture_infos
+
+
+def getDayHeader(day_str):
+    return "{}".format(day_str).capitalize().center(28, '+') + "\n"
+
+
+def createTimetable():
+    raw_tt = getRawTimetable()
+    if len(raw_tt) == 0: return {}  # In case of error
+    raw_tt = raw_tt["courses"]
+    # Setup timetable
+    timetable = {
+        "monday": [],
+        "tuesday": [],
+        "wednesday": [],
+        "thursday": [],
+        "friday": [],
+        "saturday": [],
+        "sunday": []
+    }
+    # Fill timetable
+    for course in raw_tt:
+        for lecture in course["timetable"]:
+            timetable[lecture["day"]].append(
+                getLectureInfos(course["acronym"], lecture))  # Add course string to that day
+    # Sort lectures by time
+    for day_lectures in timetable.values():
+        if len(day_lectures) > 0:
+            day_lectures.sort(key=lambda date_str: dt.datetime.strptime(date_str.split('.')[0].split('-')[0], "%H:%M"))     # Sort by the start hour
+    # Join in a single string
+    for day_str in timetable:
+        timetable[day_str] = "".join(timetable[day_str])  # Join sorted day lectures
+        if len(timetable[day_str]) > 0:
+            timetable[day_str] = getDayHeader(day_str) + timetable[day_str]  # Add day header
+    return timetable
+
+
+##### DAYS CONVERSION #########################
+days = [
+    ('Dom - Sun', 'sunday'),
+    ('Lun - Mon', 'monday'),
+    ('Mar - Tue', 'tuesday'),
+    ('Mer - Wed', 'wednesday'),
+    ('Gio - Thu', 'thursday'),
+    ('Ven - Fri', 'friday'),
+    ('Sab - Sat', 'saturday')
+]
+
+
+def getDayCode(day_msg):
+    count = 0
+    for day in days:
+        if day_msg in day:
+            return count
+        else:
+            count += 1
+    return -1   # In case of error
+
+
+def getLecturesByDay(day_code):
+    lects = timetable[days[day_code][1]]
+    if len(lects) != 0:
+        return lects
+    else:
+        return '*No Lectures!*'
+
 
 #####FUNZIONI BOT#############################
 def start(bot, update):
-	user = update.message.from_user
-	update.message.reply_text('Bot started!')
+    user = update.message.from_user
+    update.message.reply_text('Bot started!')
 
 
-#>> Comando Orario
-def orari(bot, update):
-	#Creo una tastiera personalizzata:
-	reply_keyboard = [['Oggi\nToday','Domani\nTomorrow'],['Select...']]
-	update.message.reply_text('Select day:',	reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-	#return il codice dello stato successivo
-	return SELECT
+# >> Custom Commands
+# TC 1 (Timetable Conversation)
+def startTimetableConversation(bot, update):
+    reply_keyboard = [['Oggi\nToday', 'Domani\nTomorrow'], ['Select...']]
+    update.message.reply_text('Select day:', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return SELECT   # Next state code
 
 
+# TC 2
+def switchMod(bot, update):
+    if update.message.text == 'Select...':
+        return chooseDay(update)  # ask to select the day
+    else:
+        return day(bot, update)  # reply to today or tomorrow request
 
-def switchMod(bot,update):
-	if update.message.text == 'Select...' :
-		return chooseDay(update)	#chiedo di selezionare il giorno
-	else:
-		return day(bot,update)		#se ho scelto oggi o domani posso già rispondere
 
-
+# TC 2.1
 def chooseDay(update):
-	#Creo una tastiera personalizzata:
-	reply_keyboard = [[mon,tue],[wed,thu],[fri]]
-	update.message.reply_text('Select the day:', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-	#return il codice dello stato successivo
-	return CHOOSE
-
-def daychoosed(bot,update):
-	request = setupDayCode(update.message.text)
-	text= getTimetable(0,request)
-	reply_markup= telegram.ReplyKeyboardRemove()
-	update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
-	return ConversationHandler.END
+    reply_keyboard = [[days[1][0], days[2][0]], [days[3][0], days[4][0]], [days[5][0]]]    # Days[0] == Sunday
+    update.message.reply_text('Select the day:',
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return CHOOSE
 
 
+# TC 2.2
+def daychoosed(bot, update):
+    day_code = getDayCode(update.message.text)
+    text = getLecturesByDay(day_code)
+    reply_markup = telegram.ReplyKeyboardRemove()
+    update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
+    return ConversationHandler.END
+
+
+# TC 3
 def day(bot, update):
-	now= datetime.datetime.now()
-	today= now.strftime('%w')
-	request= getDayCode(update.message.text)
-	text= getTimetable(today,request)
-	reply_markup= telegram.ReplyKeyboardRemove()
-	update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
-	return ConversationHandler.END
-#<<
+    if "Oggi" in update.message.text:
+        day_code = int(dt.datetime.now().strftime('%w'))
+    else:
+        day_code = int(dt.datetime.now().strftime('%w'))
+        day_code = (day_code + 1) % 7
+    text = getLecturesByDay(day_code)
+    reply_markup = telegram.ReplyKeyboardRemove()
+    update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
+    return ConversationHandler.END
+
+# <<
 
 def error(bot, update, error):
+    pass
 
 
-def cancel(bot,update):
-	user = update.message.from_user
-	reply_markup= telegram.ReplyKeyboardRemove()
-	update.message.reply_text('Command aborted!',reply_markup= reply_markup)
-	return ConversationHandler.END
+def cancel(bot, update):
+    user = update.message.from_user
+    reply_markup = telegram.ReplyKeyboardRemove()
+    update.message.reply_text('Command aborted!', reply_markup=reply_markup)
+    return ConversationHandler.END
 
 
 def unknown(bot, update):
-	user = update.message.from_user
-	update.message.reply_text('Unknown command!\nPress /timetable to start!')
+    user = update.message.from_user
+    update.message.reply_text('Unknown command!\nPress /timetable to start!')
 
 
-
-#Definisco gli handler
+# Definisco gli handler
 start_handler = CommandHandler('start', start)
 
 timetable_handler = ConversationHandler(
-		entry_points=[CommandHandler('timetable',orari)],
-		
-		states={
-			SELECT: [MessageHandler(Filters.text, switchMod)],
-			CHOOSE: [MessageHandler(Filters.text, daychoosed)]
-				},
+    entry_points=[CommandHandler('timetable', startTimetableConversation)],
 
-		fallbacks=[CommandHandler('cancel',cancel)]
-		)
+    states={
+        SELECT: [MessageHandler(Filters.text, switchMod)],
+        CHOOSE: [MessageHandler(Filters.text, daychoosed)]
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 
-
-#Aggiungo gli Handler al dispatcher
+# Aggiungo gli Handler al dispatcher
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(timetable_handler)
 dispatcher.add_error_handler(error)
 dispatcher.add_handler(unknown_handler)
 
-#Start the BOT
+# Start the BOT
+timetable = createTimetable()
 updater.start_polling()
+print("Timetable Bot Started!")
